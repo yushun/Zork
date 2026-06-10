f80a3cd67d17c4b9852029061ace87d581d5334c
Yushun He <sonnyhe2002@gmail.com>
2026-06-10 16:33:26 +0000

    Better UI

--- app/javascript/game/init.js
+++ app/javascript/game/init.js
@@ -6,7 +6,7 @@ import { renderMinimap } from 'game/minimap';
 export const initializeGameView = () => {
   const commandField = document.querySelector('.command-field');
   const commandForm = document.querySelector('.command-form');
-  const output = document.querySelector('.terminal-output');
+  const output       = document.querySelector('.history-output');
 
   bindTranscriptAutoScroll(output);
   scheduleScrollToBottom(output, consumePendingScroll());
